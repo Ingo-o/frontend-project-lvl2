e44f58f542e59da9d3f411801881fd905b111630
Ingo <ingo-o@yandex.ru>
2020-12-26 19:06:20 +0300

    change imports in index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,14 @@
 import fs from 'fs';
-import getDiff from '../src/index.js';
-import path from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
 test('compare two flat jsons', () => {
   expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json'))
-  .toEqual(fs.readFileSync(getFixturePath('expected-file.txt'), 'utf-8'));
+    .toEqual(fs.readFileSync(getFixturePath('expected-file.txt'), 'utf-8'));
 });
