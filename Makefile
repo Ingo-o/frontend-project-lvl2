878daa83a2346bfb5eaa3b0692b837fd7884e922
Ingo <ingo-o@yandex.ru>
2020-12-26 19:00:08 +0300

    code coverage first attempt

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ lint:
 	npx eslint .
 
 test:
-	npx -n --experimental-vm-modules jest
+	npx -n --experimental-vm-modules jest 
 
 test-coverage:
 	npx -n --experimental-vm-modules jest --coverage
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,15 @@
+import fs from 'fs';
 import getDiff from '../src/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('compare two flat jsons', () => {
-  expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
-    `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`,
-  );
+  expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json'))
+  .toEqual(fs.readFileSync(getFixturePath('expected-file.txt'), 'utf-8'));
 });
