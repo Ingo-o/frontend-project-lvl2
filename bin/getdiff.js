067d3d183af1e3129b9faac4009eef003f4046f6
Ingo <ingo-o@yandex.ru>
2021-01-06 17:00:46 +0300

    working with formatters

--- bin/getdiff.js
+++ bin/getdiff.js
@@ -3,15 +3,14 @@
 import program from 'commander';
 import getDiff from '../src/index.js';
 import parsers from '../src/parsers.js';
-import formatter from '../src/formatters/formatter.js'
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(formatter(getDiff(parsers(filepath1), parsers(filepath2))));
+    console.log(getDiff(parsers(filepath1), parsers(filepath2), program.format));
   });
 
 program.parse(process.argv);
