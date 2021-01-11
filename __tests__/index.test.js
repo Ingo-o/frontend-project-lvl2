0f92a3038367c6631ddbf49e5e8fdb0a4d1b9ac5
Ingo <ingo-o@yandex.ru>
2021-01-11 16:42:30 +0300

    workin on the six step

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,12 +8,12 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-test('compare two flat jsons', () => {
-  expect(getDiff('__fixtures__/flat-1.json', '__fixtures__/flat-2.json'))
-    .toEqual(fs.readFileSync(getFixturePath('expected-flat.txt'), 'utf-8'));
+test('comparing 1.json and 2.yml', () => {
+  expect(getDiff('__fixtures__/1.json', '__fixtures__/2.yml'))
+    .toEqual(fs.readFileSync(getFixturePath('expected-stylish.txt'), 'utf-8'));
 });
 
-test('compare two flat yamls', () => {
-  expect(getDiff('__fixtures__/flat-1.yaml', '__fixtures__/flat-2.yml'))
-    .toEqual(fs.readFileSync(getFixturePath('expected-flat.txt'), 'utf-8'));
+test('comparing 1.yaml and 2.json', () => {
+  expect(getDiff('__fixtures__/1.yaml', '__fixtures__/2.json'))
+    .toEqual(fs.readFileSync(getFixturePath('expected-stylish.txt'), 'utf-8'));
 });
--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
-import _ from 'lodash';
+import getDiff from './getdiff.js';
+import parsers from './parsers.js';
+import format from './formatters/stylish.js';
 
-const getDiff = (file1, file2) => {
-
-  const keys1 = Object.keys(file1);
-  const keys2 = Object.keys(file2);
-
-  // Объединяем ключи в один массив => удаляем повторяющиеся => сортируем
-  const jointKeys = _.sortBy(_.uniq(keys1.concat(keys2)));
-
-  let result = [];
-
-  jointKeys.forEach((key) => {
-    // Ключ есть во втором, а в первом нет (ADDED)
-    if (keys2.includes(key) && !keys1.includes(key)) {
-      result.push({name: key, type: 'ADDED', value: file2[key]});
-      // Ключ есть в первом, а во втором нет (REMOVED)
-    } else if (keys1.includes(key) && !keys2.includes(key)) {
-      result.push({name: key, type: 'REMOVED', value: file1[key]});
-      // Ключ есть в обоих
-    } else if (keys1.includes(key) && keys2.includes(key)) {
-        if (_.isObject(file1[key]) && _.isObject(file2[key])) {
-          result.push({name: key, type: 'PARENT', value: getDiff(file1[key], file2[key])});
-        } else if (file1.key === file2.key) {
-          result.push({name: key, type: 'UNCHANGED', value: file1[key]});
-        } else if (file1.key !== file2.key) {
-          result.push({name: key, type: 'CHANGED', oldValue: file1[key], newValue: file2[key]});
-        }
-    }
-  });
-
-  return result;
-  
+export default (filepath1, filepath2, formatName = 'stylish') => {
+  const file1 = parsers(filepath1);
+  const file2 = parsers(filepath2);
+  return format(getDiff(file1, file2));
 };
 
-export default getDiff;
