fd2da248f5816e54b34d3b7bd4166ca4585eab06
Ingo <ingo-o@yandex.ru>
2021-01-22 16:27:07 +0300

    working n the plain-formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -3,28 +3,31 @@ import _ from 'lodash';
 // Вспомогательная функция для обработки значений
 const valueFormatter = (value) => {
   if (!_.isObject(value)) {
-    return value;
+    const formattedValue = _.isString(value) ? `'${value}'` : `${value}`;
+    return formattedValue;
   }
 
-  return `[complex value]`;
+  return '[complex value]';
 };
 
 // Функция формирует plain-вывод в консоль на основе результата из getDiff
 const formatter = (data) => {
-  console.log(`${JSON.stringify(data, null, 2)}\n--------------------------------------------------------------------`)
   const formattedData = data.map(({
     name, type, value, oldValue, newValue, children,
   }) => {
     if (type === 'ADDED') {
-      return `Property 'TEST' was added with value: ${valueFormatter(value)}`;
+      return `Property '${name}' was added with value: ${valueFormatter(value)}`;
     } if (type === 'REMOVED') {
-      return `Property 'TEST' was removed`;
+      return `Property '${name}' was removed`;
     } if (type === 'CHANGED') {
-      return `Property 'TEST was updated. From ${valueFormatter(oldValue)} to ${valueFormatter(newValue)}`;
+      return `Property '${name}' was updated. From ${valueFormatter(oldValue)} to ${valueFormatter(newValue)}`;
+    } if (type === 'PARENT') {
+      return `${formatter(children)}`;
     }
-    // if (type === 'PARENT') - дефолтное условие что бы линтер не ругался :-\
-    return `${formatter(children)}`;
-  });  
+
+    return 'The enemies of the Emperor shall be destroyed!';
+  })
+    .filter((elem) => elem !== 'The enemies of the Emperor shall be destroyed!');
 
   return `${formattedData.join('\n')}`;
 };
